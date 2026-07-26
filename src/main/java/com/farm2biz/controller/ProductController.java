bea227f2b4ac06d1b3ee0fcdc5b8ca54c93ce055
Amit Kumar Dubey <amitnew35@gmail.com>
2026-07-26 17:27:17 +0530

    modify and update ProductController.java

--- src/main/java/com/farm2biz/controller/ProductController.java
+++ src/main/java/com/farm2biz/controller/ProductController.java
@@ -25,33 +25,33 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 
 	private final ProductService productService;
-
+//add product
 	@PostMapping
 	public ResponseEntity<?> createProduct(@RequestBody @Valid ProductDTO dto) {
 		System.out.println("in create product " + dto);
 		ProductDTO created = productService.createProduct(dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(created);
 	}
-
+//get product by Id
 	@GetMapping("/{productId}")
 	public ResponseEntity<?> getProductById(@PathVariable Long productId) {
 		System.out.println("in get product by id " + productId);
 		return ResponseEntity.ok(productService.getProductById(productId));
 	}
-
+//get all products
 	@GetMapping
 	public ResponseEntity<?> getAllProducts() {
 		System.out.println("in get all products");
 		List<ProductDTO> products = productService.getAllProducts();
 		return ResponseEntity.ok(products);
 	}
-
+//update products
 	@PutMapping("/{productId}")
 	public ResponseEntity<?> updateProduct(@PathVariable Long productId, @RequestBody @Valid ProductDTO dto) {
 		System.out.println("in update product " + productId + " " + dto);
 		return ResponseEntity.ok(productService.updateProduct(productId, dto));
 	}
-
+//delete product
 	@DeleteMapping("/{productId}")
 	public ResponseEntity<?> deleteProduct(@PathVariable Long productId) {
 		System.out.println("in delete product " + productId);
